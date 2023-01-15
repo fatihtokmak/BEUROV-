1f87819a7b7d328278a8a5254c5fb3d6fcdacc07
fatihtokmak <108019837+fatihtokmak@users.noreply.github.com>
2023-01-15 18:40:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="TR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anasayfa  |  BEUROV</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://kit.fontawesome.com/642dde49e1.js" crossorigin="anonymous"></script>
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2? family=Nunito&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+
+</head>
+<body>
+
+    <section id="menu">
+        <div id="logo">BEUROV</div>
+        <nav>
+            <a href=""><i class="fa-solid fa-house-user dayi"></i>Anasayfa</a>
+            <a href=""><i class="fa-solid fa-info dayi"></i>Hakkımızda</a>
+            <a href=""><i class="fa-solid fa-user-graduate dayi"></i>Projeler</a>
+            <a href=""><i class="fa-solid fa-user-group dayi"></i>Ekip</a>
+            <a href=""><i class="fa-solid fa-phone dayi"></i>İletişim</a>
+        </nav>
+    </section>
+
+    <section id="anasayfa">
+        <div id="black">
+        </div>
+        <div id="icerik">
+            <h2>Proje Labaratuvarımız</h2>
+            <hr width=300 align: left>
+             Vedat Tümen aracılığı ile Rektörlüğün bize projelerimizde daha rahat çalışabilmemiz için sunduğu proje labaratuvarımız.
+             Labaratvarda daha çok yazılımsal işleri yapmakta olup Backend kısımlarını burada yapmaktayız.
+        </div>
+    <section id="hakkimizda">
+        <h3> Hakkımızda</h3>
+        <div class ="container">
+            <div id="sol">
+                <h5 id="h5sol">Bitlis Eren üniversitesi 3. sınıf öğrencileriyiz. 
+                    Daha Çok teknofest projeleri ve benzeri arac tasarımı ile ilgili projelerle ilgileniyoruz.
+                </h5>
+            </div>
+
+            <div id="sag">
+                <span></span>
+                <p id="psag">Yazılım alanında kendimizi geliştirmek için teknofest projelerine katıldık.
+                    sadece yazılım alanıyla da kalmayıp kendimizi elektronik ve mekanik tasarımlarda da geliştirdik.
+                </p>
+            </div>
+            <img src="img/2.JPG" alt=""
+            class="img-fluid mt100">
+            <p id="pson">Bütün Malzemelerimiz üniversite hocalarımızdan Vedat Tümen tarafından karşılanmıştır. 
+                Vedat Tümen aynı zamanda proje ekibimizin danışmanıdır.
+                Projemizde elektronik ve meknaik işler bitmiş olup sadece yazılımsal işler kalmıştır buda en kısa zamanda yapılıp proje bitmiş durumuna getirilecektir. </p>
+        </div>
+    </section>
+    <section id="projeler">
+        <div class="container">
+            <h3>Projeler</h3>
+            <div>
+                <div class="card">
+                    <img src="img/10.jpg" alt="" class="img-fluid">
+                    <h5
+                    class="basslikcard">İSA</h5>
+                    <p class="cardp">Tenofest İnsansız su altı yarışmasında kullanacağımız aracımız.</p>
+                </div>
+                <div class="card">
+                    <img src="img/6.jpg" alt="" class="img-fluid">
+                    <h5
+                    class="basslikcard">İHA</h5>
+                    <p class="cardp">Kendi ekibimizin oluşturduğu insansız hava aracımız.</p>
+                </div>
+                <div class="card">
+                    <img src="img/8.jpg" alt="" class="img-fluid">
+                    <h5
+                    class="basslikcard">SİHA</h5>
+                    <p class="cardp">Savaşan iha yarışmasında kullanacağımız aracımız.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="ekip">
+        <div class="container">
+            <h3 id="ekiph3">Ekip</h3>
+
+            <div class="sutun-sol-sag">
+                <img src="img/1.jpg" alt="" class="img-f oval">
+                <h4 class="ekipisim">Fatih Tokmak</h4>
+                <P class="ekipp">
+                Bilgisayar Mühendisliği 3. sınıf öğrencisi 
+                Projede daha çok Elektriksel işlerle uğraşır.
+                </P>
+                <div class="ekip-dayi">
+                    <a href=""><i class="fa-brands fa-square-facebook social"></i></a>
+                    <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+                    <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+                </div>
+                
+            </div>
+            <div class="sutun-sol-sag">
+                <img src="img/4.jpg" alt="" class="img-f oval">
+                <h4 class="ekipisim">Melike Turan</h4>
+                <P class="ekipp">
+                Bilgisayar Mühendisliği 3. sınıf öğrencisi 
+                Projede daha çok yazılımsal işlerle ilgilenir.
+                </P>
+                <div class="ekip-dayi">
+                    <a href=""><i class="fa-brands fa-square-facebook social"></i></a>
+                    <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+                    <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+                </div>
+                
+            </div>
+            <div class="sutun-sol-sag">
+                <img src="img/5.JPG" alt="" class="img-f oval">
+                <h4 class="ekipisim">Mithat Berhan Zeydan</h4>
+                <P class="ekipp">
+                    Bilgisayar Mühendisliği 3. sınıf öğrencisi 
+                    Projede daha çok mekanik ve donanım işlerine bakar.
+                </P>
+                <div class="ekip-dayi">
+                    <a href=""><i class="fa-brands fa-square-facebook social"></i></a>
+                    <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+                    <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <section id="iletisim">
+        <div class="container">
+            <h3 id="h3iletisim">İletişim</h3>
+
+            <form action="index.php" method="post">
+            <div id="iletisimopak">
+
+                <div id="formgroup">
+                <div id="solform">
+                    <input type="text" 
+                    name="isim" 
+                    placeholder="Ad Soyad" 
+                    required class="form-control">
+                    <input type="text" 
+                    name="tel" 
+                    placeholder="Telefon" 
+                    required class="form-control">
+                </div>
+                <div id="sagform">
+                    <input type="email" 
+                    name="mail" 
+                    placeholder="Email Adresiniz" 
+                    required class="form-control">
+                    <input type="text" 
+                    name="konu" 
+                    placeholder="Konu Başlığı" 
+                    required class="form-control">
+                </div>
+                <textarea name="mesaj" 
+                id="" cols="30" 
+                placeholder="Mesaj Girin" 
+                rows="10" 
+                required class="form-control"></textarea>
+                <input type="submit" value="Gönder">
+            </div>
+            <div id="adres">
+                <h4 id="adresbaslik">Adres :</h4>
+                <p class="adresp"> Fatih Mahallesi</p>
+                <p class="adresp">Fatih Sokak</p>
+                <p class="adresp">No=12</p>
+                <p class="adresp">05522358983</p>
+                <p class="adresp">Email : fatihtkmk21.23@gmail.com</p>
+            </div>
+        </div>
+            </form>
+    </div>
+
+        <footer>
+            <div id="copyright">2023 Tüm Haklar Saklıdır.</div>
+            <div class="socialfooter">
+                <a href=""><i class="fa-brands fa-square-facebook social"></i></a>
+                <a href=""><i class="fa-brands fa-square-instagram social"></i></a>
+                <a href=""><i class="fa-brands fa-square-twitter social"></i></a>
+            </div>
+        </footer>
+    </div>
+    </section>
+    <script src="owl/owl.carousel.min.js">
+        
+    </script>  
+</body>
+</html>
+
+
+<?php
+
+include("baglanti.php");
+
+if(isset($_POST["isim"], $_POST["tel"], $_POST["mail"], 
+$_POST["konu"], $_POST["mesaj"]))
+{
+    $adsoyad=$_POST["isim"];
+    $telefon=$_POST["tel"];
+    $email=$_POST["mail"];
+    $konu=$_POST["konu"];
+    $mesaj=$_POST["mesaj"];
+
+    $ekle = "INSERT INTO iletisim(adsoyad, telefon, 
+    email, konu, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+
+    if($baglan->query($ekle)===TRUE)
+    {
+        echo "<script>alert('Mesajınız Başarı İle Gönderilmiştir.')</script>";
+    }
+    else{
+        echo "<script>alert('Mesajınız Gönderilirken hata oluştu.')</script>";
+    }
+}
+?>
